6af45b29c1d34034688777f1eb78844723886cf1
payamaya <paulyashou81@gmail.com>
2026-09-12 15:43:15 +0200

    create a person variables and print  to the console.log()

--- uppgift1.js
+++ uppgift1.js
@@ -1,1 +1,9 @@
 'use strict'
+const firstName = 'Malin'
+const lastName = 'Larsson'
+let age = 30
+let isStudent = false
+
+console.log(`${firstName}" "${lastName}`)
+console.log(`Ålder: ${age}`)
+console.log(`${isStudent}`)
